2eee23ff31cc97a56e6bc1aec4c2412039ed0be9
Hiago Freitas <h07.machado@gmail.com>
2025-01-20 17:25:21 +0000

    form npm run dev

--- servidor/src/models/Formacao.js
+++ servidor/src/models/Formacao.js
@@ -1,4 +1,3 @@
-// src/models/Formacao.js
 const Sequelize = require('sequelize');
 const sequelize = require('../database');
 
@@ -11,9 +10,20 @@ const Formacao = sequelize.define('formacao', {
     nome: {
         type: Sequelize.STRING,
         allowNull: false,
+        unique: true, // Garante que não haja duplicatas
     }
 }, {
     timestamps: false,
 });
+
+Formacao.initDefaultFormacoes = async () => {
+    const defaultFormacoes = [
+        "3-4-3", "3-5-2", "4-3-3", "4-2-4", "4-4-2", "4-2-3-1", "5-2-3", "5-3-2", "5-4-1"
+    ];
+
+    for (const nome of defaultFormacoes) {
+        await Formacao.findOrCreate({ where: { nome } });
+    }
+};
 
 module.exports = Formacao;
