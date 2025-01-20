f2219f6f4a5d437596ed03567b8ab0c0ab2efca1
s0ares999 <pedro_mdsoares@hotmail.com>
2025-01-20 23:39:43 +0000

    Update database.js

--- servidor/src/database.js
+++ servidor/src/database.js
@@ -4,7 +4,7 @@ const sequelize = new Sequelize(
     'pi4_bd_qupu_user',
     'F7oAZKjvTctKSZtOf7oyWHskZcOUB9QZ',
     {
-        host: 'dpg-cu7a8hqj1k6c73fjeqng-a.render.com',
+        host: 'dpg-cu7a8hqj1k6c73fjeqng-a.frankfurt-postgres.render.com',
         port: '5432',
         dialect: 'postgres'
     }
