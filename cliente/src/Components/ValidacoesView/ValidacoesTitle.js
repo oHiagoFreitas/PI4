65d7a965b355658c88a0d14ee987208be2faa5f8
Hiago Freitas <h07.machado@gmail.com>
2025-01-20 18:38:33 +0000

    Nomeclatura

--- cliente/src/Components/ValidacoesView/ValidacoesTitle.js
+++ cliente/src/Components/ValidacoesView/ValidacoesTitle.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import '../../Style/AtletasTitle.css';
 
-const ValidacoesTitle = () => { // Altere o nome aqui
+const ValidacoesTitle = () => {
     return (
         <div className="AtletasTitleMessage">
-            <h1>Validar Times, Atletas, Relatórios e Utilizadores</h1>
+            <h1>Validar Equipas, Atletas, Relatórios e Utilizadores</h1>
         </div>
     );
 };
