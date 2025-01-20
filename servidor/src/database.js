60937b3cb24436f81161040e17246b93433ab655
s0ares999 <pedro_mdsoares@hotmail.com>
2025-01-20 23:21:19 +0000

    database

--- servidor/src/database.js
+++ servidor/src/database.js
@@ -1,10 +1,10 @@
 var Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-    'API4',
-    'postgres',
-    'postgres',
+    'pi4_bd_qupu',
+    'pi4_bd_qupu_user',
+    'F7oAZKjvTctKSZtOf7oyWHskZcOUB9QZ',
     {
-        host: 'localhost',
+        host: 'dpg-cu7a8hqj1k6c73fjeqng-a.render.com',
         port: '5432',
         dialect: 'postgres'
     }
