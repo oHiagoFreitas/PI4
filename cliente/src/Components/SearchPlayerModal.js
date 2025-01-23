140ee71d989eceba71d4089cf5494991e391baf9
Hiago Freitas <h07.machado@gmail.com>
2025-01-23 00:44:40 +0000

    a

--- cliente/src/Components/SearchPlayerModal.js
+++ cliente/src/Components/SearchPlayerModal.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Modal from 'react-modal';
-import '../Style/MMJ_PFR.css'; // Supondo que você coloque os estilos acima em ModalStyles.css
 
 const SearchPlayerModal = ({ onPlayerSelect }) => {
   const [jogadores, setJogadores] = useState([]);
@@ -29,39 +28,37 @@ const SearchPlayerModal = ({ onPlayerSelect }) => {
 
   return (
     <div>
-      <button onClick={openModal} className="MMJ_PFR_submit-button">Selecione o Jogador</button>
+      <button onClick={openModal} className="submit-button">Selecione o Jogador</button>
 
       <Modal
         isOpen={isModalOpen}
         onRequestClose={closeModal}
-        contentLabel="Jogadores Aprovados"
+        contentLabel="Jogadores"
         ariaHideApp={false}
-        className="MMJ_PFR_ReactModal__Content"
-        overlayClassName="MMJ_PFR_ReactModal__Overlay"
       >
         <h2>Jogadores Aprovados</h2>
-        <button onClick={closeModal} className="MMJ_PFR_button-close">Fechar</button>
+        <button onClick={closeModal}>Fechar</button>
         <div style={{ maxHeight: '400px', overflowY: 'auto' }}>
-          <table className="MMJ_PFR_table">
+          <table border="1" style={{ width: '100%' }}>
             <thead>
               <tr>
-                <th className="MMJ_PFR_th">Nome</th>
-                <th className="MMJ_PFR_th">Data de Nascimento</th>
-                <th className="MMJ_PFR_th">Posição</th>
-                <th className="MMJ_PFR_th">Clube</th>
-                <th className="MMJ_PFR_th">Link</th>
-                <th className="MMJ_PFR_th">Ação</th>
+                <th>Nome</th>
+                <th>Data de Nascimento</th>
+                <th>Posição</th>
+                <th>Clube</th>
+                <th>Link</th>
+                <th>Ação</th>
               </tr>
             </thead>
             <tbody>
               {jogadores.map(jogador => (
                 <tr key={jogador.id}>
-                  <td className="MMJ_PFR_td">{jogador.nome}</td>
-                  <td className="MMJ_PFR_td">{new Date(jogador.dataNascimento).toLocaleDateString()}</td>
-                  <td className="MMJ_PFR_td">{jogador.posicao}</td>
-                  <td className="MMJ_PFR_td">{jogador.clube}</td>
-                  <td className="MMJ_PFR_td"><a href={jogador.link} target="_blank" rel="noopener noreferrer">Perfil</a></td>
-                  <td className="MMJ_PFR_td">
+                  <td>{jogador.nome}</td>
+                  <td>{new Date(jogador.dataNascimento).toLocaleDateString()}</td>
+                  <td>{jogador.posicao}</td>
+                  <td>{jogador.clube}</td>
+                  <td><a href={jogador.link} target="_blank" rel="noopener noreferrer">Perfil</a></td>
+                  <td>
                     <button onClick={() => selecionarJogador(jogador)}>Selecionar</button>
                   </td>
                 </tr>
