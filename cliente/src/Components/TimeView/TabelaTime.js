f7df96698f9742a5351e03a383dd123951b1603d
Hiago Freitas <h07.machado@gmail.com>
2025-01-23 03:33:56 +0000

    Update TabelaTime.js

--- cliente/src/Components/TimeView/TabelaTime.js
+++ cliente/src/Components/TimeView/TabelaTime.js
@@ -35,7 +35,7 @@ function TabelaTimes({ times, handleEdit, handleDelete }) {
     setIsModalOpen(false);
     setSelectedTeam(null);
     if (refresh) {
-        window.location.href = "https://frontpi4.onrender.com/times";  // Redireciona para a página inicial
+        window.location.href = "/";  // Redireciona para a página inicial
     }
 };
 
